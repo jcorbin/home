f8615d39b90a70c0bb86dfac1b6ef3fe56fafdfe
Joshua T Corbin <jcorbin@wunjo.org>
2022-11-21 11:01:30 -0500

    nvim: set termguicolors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,8 @@ local defered = function(callback)
   end
 end
 
+opt.termguicolors = true
+
 local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim' -- {{{
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({ 'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path })
