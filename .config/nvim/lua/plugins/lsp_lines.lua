1b00f8aaf6fd9eaacd8dbe08553c61e887ed8393
Joshua T Corbin <jcorbin@wunjo.org>
2023-01-28 10:25:47 -0500

    neovim: fix lsp_lines setup

--- .config/nvim/lua/plugins/lsp_lines.lua
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -2,10 +2,9 @@ local mykeymap = require 'my.keymap'
 
 return {
   url = 'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-  opts = {},
-  config = function(_, opts)
+  config = function()
     local lsp_lines = require 'lsp_lines'
-    lsp_lines.setup(opts)
+    lsp_lines.setup()
     vim.diagnostic.config({ virtual_text = false, })
     mykeymap.leader('n', 'l', lsp_lines.toggle)
   end
