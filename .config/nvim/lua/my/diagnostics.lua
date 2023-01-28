b64c49a05a5036b68cb18a174eb926d902ac9c91
Joshua T Corbin <jcorbin@wunjo.org>
2023-01-28 10:26:52 -0500

    neovim: document diagnostic keymaps

--- .config/nvim/lua/my/diagnostics.lua
+++ .config/nvim/lua/my/diagnostics.lua
@@ -16,12 +16,13 @@ vim.diagnostic.config {
 
 mykeymap.pair('n', 'q',
   bind(vim.cmd, 'cprev'),
-  bind(vim.cmd, 'cnext'))
+  bind(vim.cmd, 'cnext'),
+  { desc = 'error (quickfix)' })
 
 mykeymap.pair('n', 'd',
   vim.diagnostic.goto_prev,
-  vim.diagnostic.goto_next)
+  vim.diagnostic.goto_next,
+  { desc = 'diagnostic' })
 
 -- TODO dedupe diagnostic open mappings
-mykeymap.leader('n', 'e', vim.diagnostic.open_float)
-mykeymap.leader('n', 'dg', vim.diagnostic.open_float)
+mykeymap.leader('n', 'dg', vim.diagnostic.open_float, { desc = 'Open diagnostics float' })
