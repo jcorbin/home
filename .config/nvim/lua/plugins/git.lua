7679d6d21b13d82f1557533b567f2db71eecbcda
Joshua T Corbin <jcorbin@wunjo.org>
2023-01-28 10:26:05 -0500

    neovim: document git keymaps

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,17 +4,17 @@ return {
   'tpope/vim-fugitive',
 
   config = function()
-    mykeymap.leader('n', 'Gg', ':G<cr>')
-    mykeymap.leader('n', 'GG', ':G<cr>')
-    mykeymap.leader('n', 'Gd', ':Gdiff<cr>')
-    mykeymap.leader('n', 'Ga', ':G add %<cr>')
-    mykeymap.leader('n', 'GA', ':G add --update<cr>')
-    mykeymap.leader('n', 'Gr', ':G reset<cr>')
-    mykeymap.leader('n', 'Gb', ':G blame<cr>')
-    mykeymap.leader('n', 'Gc', ':G commit<cr>')
-    mykeymap.leader('n', 'GC', ':G commit --amend<cr>')
+    mykeymap.leader('n', 'Gg', ':G<cr>', { desc = 'Git Dashboard' })
+    mykeymap.leader('n', 'GG', ':G<cr>', { desc = 'Git Dashboard' })
+    mykeymap.leader('n', 'Gd', ':Gdiff<cr>', { desc = 'Git diff' })
+    mykeymap.leader('n', 'Ga', ':G add %<cr>', { desc = 'Git add buffer' })
+    mykeymap.leader('n', 'GA', ':G add --update<cr>', { desc = 'Git add update' })
+    mykeymap.leader('n', 'Gr', ':G reset<cr>', { desc = 'Git reset' })
+    mykeymap.leader('n', 'Gb', ':G blame<cr>', { desc = 'Git blame' })
+    mykeymap.leader('n', 'Gc', ':G commit<cr>', { desc = 'Git commmit' })
+    mykeymap.leader('n', 'GC', ':G commit --amend<cr>', { desc = 'Git commmit amend' })
     mykeymap.leader('n', 'Go',
       -- TODO implement a function that avoids clobbering the default register
-      'yaw:Gsplit <C-r>"<cr>')
+      'yaw:Gsplit <C-r>"<cr>', { desc = 'Git open object' })
   end
 }
