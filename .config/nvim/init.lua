d3d350d6821822c004eda8bc7a769719c466c3df
Joshua T Corbin <jcorbin@wunjo.org>
2023-02-04 12:39:52 -0500

    neovim: break my.options out of init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,9 +9,9 @@ local bind = function(f, ...)
 end
 
 local mykeymap = require 'my.keymap'
-local augroup = require 'my.augroup'
 
 -- group for ungrouped autocmds so that they are deduped when reloading
+local augroup = require 'my.augroup'
 local autocmd = augroup 'myvimrc'
 
 -- context marker motion
@@ -46,75 +46,7 @@ mykeymap.leader({ 'n', 'v' }, 'vd', [[:v\/ delete<cr>]])
 autocmd('TextYankPost', function() vim.highlight.on_yank { timeout = 500 } end)
 
 require 'my.language_servers'
-
--- Options {{{
-
-local opt = vim.opt
-
-opt.guifont = 'JetBrains Mono:h12'
-
-opt.termguicolors = true
-opt.background = 'dark'
-
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.expandtab = true
-
-opt.breakindent = true
-
-opt.cursorline = true
-
-opt.mouse = 'a'
-opt.scrolloff = 2
-
-opt.incsearch = true
-opt.smartcase = true
-
-opt.virtualedit = 'all'
-opt.laststatus = 2
-opt.updatetime = 250
-
-autocmd('FileType', {
-  'zig',
-}, 'setlocal commentstring=//\\ %s')
-
--- TODO listchars
--- opt.listchars = {
--- eol = '↲',
--- tab = '▸ ',
--- trail = '·',
--- extends
--- precedes
--- conceal
--- nbsp
--- }
-
-opt.completeopt = { 'menu', 'menuone', 'noselect' }
-
--- neovide specific config
-if vim.g.neovide then
-  vim.g.neovide_scale_factor = 1.0
-
-  local scale_step = 0.05
-  vim.keymap.set({ 'n' }, '<C-=>', function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * (1 + scale_step) end)
-  vim.keymap.set({ 'n' }, '<C-->', function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / (1 + scale_step) end)
-  vim.keymap.set({ 'n' }, '<C-0>', function() vim.g.neovide_scale_factor = 1.0 end)
-
-  vim.g.neovide_hide_mouse_when_typing = true
-
-  vim.g.neovide_cursor_vfx_mode = 'railgun'
-end
-
--- option toggles
-mykeymap.opt_toggle('<leader>ci', 'ignorecase')
-mykeymap.opt_toggle('<leader>ln', 'number')
-mykeymap.opt_toggle('<leader>rc', 'relativenumber')
-mykeymap.opt_toggle('<leader>cl', 'cursorline')
-mykeymap.opt_toggle('<leader>cc', 'cursorcolumn')
-mykeymap.opt_toggle('<leader>lw', 'wrap')
-mykeymap.opt_toggle('<leader>sp', 'spell')
-
--- }}}
+require 'my.options'
 
 -- TODO mini.exchange? mini.move? to repalce line exchange pair mapping
 -- TODO glepnir/lspsaga.nvim
@@ -126,5 +58,3 @@ mykeymap.opt_toggle('<leader>sp', 'spell')
 -- * ziglang/zig.vim
 
 -- TODO audit old vimrc for more
-
--- vim: set ts=2 sw=2 foldmethod=marker:
---
+++ .config/nvim/lua/my/options.lua
@@ -0,0 +1,72 @@
+local mykeymap = require 'my.keymap'
+
+local augroup = require 'my.augroup'
+local autocmd = augroup 'my.options'
+
+local opt = vim.opt
+
+opt.guifont = 'JetBrains Mono:h12'
+
+opt.termguicolors = true
+opt.background = 'dark'
+
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.expandtab = true
+
+opt.breakindent = true
+
+opt.cursorline = true
+
+opt.mouse = 'a'
+opt.scrolloff = 2
+
+opt.incsearch = true
+opt.smartcase = true
+
+opt.virtualedit = 'all'
+opt.laststatus = 2
+opt.updatetime = 250
+
+-- TODO break this out into a zig-specific module
+autocmd('FileType', {
+  'zig',
+}, 'setlocal commentstring=//\\ %s')
+
+-- TODO listchars
+-- opt.listchars = {
+-- eol = '↲',
+-- tab = '▸ ',
+-- trail = '·',
+-- extends
+-- precedes
+-- conceal
+-- nbsp
+-- }
+
+opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+-- neovide specific config
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 1.0
+
+  local scale_step = 0.05
+  vim.keymap.set({ 'n' }, '<C-=>',
+    function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * (1 + scale_step) end)
+  vim.keymap.set({ 'n' }, '<C-->',
+    function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / (1 + scale_step) end)
+  vim.keymap.set({ 'n' }, '<C-0>', function() vim.g.neovide_scale_factor = 1.0 end)
+
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  vim.g.neovide_cursor_vfx_mode = 'railgun'
+end
+
+-- option toggles
+mykeymap.opt_toggle('<leader>ci', 'ignorecase')
+mykeymap.opt_toggle('<leader>ln', 'number')
+mykeymap.opt_toggle('<leader>rc', 'relativenumber')
+mykeymap.opt_toggle('<leader>cl', 'cursorline')
+mykeymap.opt_toggle('<leader>cc', 'cursorcolumn')
+mykeymap.opt_toggle('<leader>lw', 'wrap')
+mykeymap.opt_toggle('<leader>sp', 'spell')
