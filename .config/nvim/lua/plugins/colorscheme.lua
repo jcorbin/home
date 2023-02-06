38347ee6ef0c3d6680db5f8ddf4a980fac38cf0e
Joshua T Corbin <jcorbin@wunjo.org>
2023-02-06 09:04:30 -0500

    neovim: switch to tokyonight colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,35 @@
 return {
   {
-    'Mofiqul/vscode.nvim',
+    'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.g.vscode_style = 'dark'
-      vim.cmd [[colorscheme vscode]]
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      light_style = "day", -- The theme is used when the background is set to light
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+      sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = true, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+    },
+    config = function(_, opts)
+      require('tokyonight').setup(opts)
+      vim.cmd [[colorscheme tokyonight]]
     end
   },
-  -- TODO restore optional/alternate colorschemes
-  -- 'rktjmp/lush.nvim';
-  -- 'tanvirtin/monokai.nvim';
-  -- 'folke/tokyonight.nvim';
-  -- 'shaunsingh/moonlight.nvim';
 }
