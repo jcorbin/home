d3af2b6739d7653531f60c1bd9b467519c1669b1
Joshua T Corbin <jcorbin@wunjo.org>
2023-02-07 12:08:04 -0500

    neovim: use treesitter for folding

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,16 @@
--- TODO reevaluate
--- opt.foldmethod = 'expr'
--- opt.foldexpr = 'nvim_treesitter#foldexpr()'
-
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
 
   opts = {
+
     ensure_installed = 'all',
+
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,
     },
+
     incremental_selection = {
       enable = true,
       keymaps = {
@@ -21,10 +20,16 @@ return {
         node_decremental = "grm",
       },
     },
+
     indent = { enable = true },
+
   },
 
   config = function(_, opts)
     require('nvim-treesitter.configs').setup(opts)
+
+    vim.opt.foldmethod = 'expr'
+    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
   end,
 }
