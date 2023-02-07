3b2506fb98d0644b513b8b75c7b3620841e960c4
Joshua T Corbin <jcorbin@wunjo.org>
2023-02-07 12:09:59 -0500

    neovim: reformat plugins.treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,34 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
 
-  opts = {
+        ensure_installed = 'all',
 
-    ensure_installed = 'all',
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
 
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "gnn",
+                node_incremental = "grn",
+                scope_incremental = "grc",
+                node_decremental = "grm",
+            },
+        },
+
+        indent = { enable = true },
+
+        textobjects = { enable = true },
+
     },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
 
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
-    },
-
-    indent = { enable = true },
-
-    textobjects = { enable = true },
-
-  },
-
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-
-    vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-
-  end,
+      vim.opt.foldmethod = 'expr'
+      vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+    end,
 }
