7dfa661c0d52a196579d475f8aa27a7d115ca02e
Joshua T Corbin <jcorbin@wunjo.org>
2023-02-13 14:29:24 -0500

    neovim: switch lua language server

--- .config/nvim/lua/my/language_servers.lua
+++ .config/nvim/lua/my/language_servers.lua
@@ -12,6 +12,29 @@ initls 'html'
 
 initls 'jsonls'
 
+initls('lua_ls', {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
+
 initls('pylsp', {
     settings = {
         -- https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
@@ -51,36 +74,6 @@ initls('pylsp', {
 
 initls 'rust_analyzer'
 -- https://github.com/rust-analyzer/rust-analyzer/tree/master/docs/user#settings
-
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-initls('sumneko_lua', {
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most
-                -- likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path,
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-})
 
 initls 'tsserver'
 
