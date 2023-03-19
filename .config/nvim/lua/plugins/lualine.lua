93a627219e0712f494e49dd5fc4e3db8e0a5487f
Joshua T Corbin <jcorbin@wunjo.org>
2023-03-19 10:37:17 -0400

    neovim: further walkback statusline experimentation

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
 local filename = {
-    'filename',
-    newfile_status = true,
-    file_status = true,
-    path = 1, -- relative path
+  'filename',
+  newfile_status = true,
+  file_status = true,
+  path = 1,   -- relative path
 }
 
 local function cwd()
@@ -10,136 +10,68 @@ local function cwd()
 end
 
 return {
-    'nvim-lualine/lualine.nvim',
-    opts = {
-
-        options = {
-            icons_enabled = true,
-            theme = 'auto',
-
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-
-            always_divide_middle = false,
-            globalstatus = false,
-
-            refresh = {
-                statusline = 1000,
-                tabline = 1000,
-                winbar = 1000,
-            }
-        },
-
-        sections = {
-            lualine_a = {
-                'mode',
-            },
-            lualine_b = {
-            },
-            lualine_c = {
-            },
-            lualine_x = {
-                'diff',
-            },
-            lualine_y = {
-                cwd,
-            },
-            lualine_z = {
-                'branch',
-            }
-        },
-
-        winbar = {
-            lualine_a = {
-                filename,
-            },
-            lualine_b = {
-                'filesize',
-                'encoding',
-                'fileformat',
-            },
-            lualine_c = {
-                'searchcount',
-                'diagnostics',
-            },
-            lualine_x = {
-            },
-            lualine_y = {
-                'progress',
-                'location',
-            },
-            lualine_z = {
-            }
-        },
-
-        inactive_winbar = {
-            lualine_a = {
-                filename,
-            },
-            lualine_b = {
-            },
-            lualine_c = {
-                'searchcount',
-                'diagnostics',
-            },
-            lualine_x = {
-            },
-            lualine_y = {
-                'location',
-            },
-            lualine_z = {
-            }
-        },
-
-        -- sections = {
-        --     lualine_a = {
-        --         filename,
-        --     },
-        --     lualine_b = {
-        --         'filesize',
-        --         'encoding',
-        --         'fileformat',
-        --     },
-        --     lualine_c = {
-        --         'searchcount',
-        --         'diagnostics',
-        --     },
-        --     lualine_x = {
-        --         'branch',
-        --         'diff',
-        --     },
-        --     lualine_y = {
-        --         'progress',
-        --         'location',
-        --     },
-        --     lualine_z = {
-        --         'mode',
-        --     }
-        -- },
-
-        -- inactive_sections = {
-        --     lualine_a = {
-        --         filename,
-        --     },
-        --     lualine_b = {
-        --     },
-        --     lualine_c = {
-        --         'searchcount',
-        --         'diagnostics',
-        --     },
-        --     lualine_x = {
-        --     },
-        --     lualine_y = {
-        --         'location',
-        --     },
-        --     lualine_z = {
-        --     }
-        -- },
-
+  'nvim-lualine/lualine.nvim',
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      always_divide_middle = false,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      }
     },
-    config = function(_, opts)
-      vim.opt.laststatus = 2
-      vim.opt.cmdheight = 1
-      require('lualine').setup(opts)
-    end,
+    sections = {
+      lualine_a = {
+        filename,
+      },
+      lualine_b = {
+        'filesize',
+        'encoding',
+        'fileformat',
+      },
+      lualine_c = {
+        'searchcount',
+        'diagnostics',
+      },
+      lualine_x = {
+        'branch',
+        'diff',
+      },
+      lualine_y = {
+        'progress',
+        'location',
+      },
+      lualine_z = {
+        'mode',
+      }
+    },
+    inactive_sections = {
+      lualine_a = {
+        filename,
+      },
+      lualine_b = {
+      },
+      lualine_c = {
+        'searchcount',
+        'diagnostics',
+      },
+      lualine_x = {
+      },
+      lualine_y = {
+        'location',
+      },
+      lualine_z = {
+      }
+    },
+  },
+  config = function(_, opts)
+    vim.opt.laststatus = 2
+    vim.opt.cmdheight = 1
+    require('lualine').setup(opts)
+  end,
 }
