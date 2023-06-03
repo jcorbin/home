158b334d69e9401ead8d6cac33d68b40489e4156
Joshua T Corbin <jcorbin@wunjo.org>
2023-06-03 10:12:02 -0400

    neovim: update treesitter scope bindings

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,10 +12,10 @@ return {
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
       },
     },
 
