1f997469b9913cce85d4e237da0b7ffc5a596f4b
Joshua T Corbin <jcorbin@wunjo.org>
2023-10-09 21:27:06 -0400

    [nvim] use telescope release branch

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ local tmap = require('my.terminal').keymap
 
 return {
   'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
