4fe91f1e2c8ea6155f0f6e4b1507774bc4f90142
Joshua T Corbin <jcorbin@wunjo.org>
2023-10-09 21:26:39 -0400

    [nvim] add glslls

--- .config/nvim/lua/my/language_servers.lua
+++ .config/nvim/lua/my/language_servers.lua
@@ -6,6 +6,15 @@ initls 'cssls'
 
 initls 'dockerls'
 
+initls('glslls', {
+  cmd = {
+    "glslls",
+    "--stdin",
+    "--target-env", "opengl",
+    -- [vulkan vulkan1.0 vulkan1.1 vulkan1.2 vulkan1.3 opengl opengl4.5]
+  },
+})
+
 initls 'gopls'
 
 initls 'html'
