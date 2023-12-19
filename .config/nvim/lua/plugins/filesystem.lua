b690174abc6c4d426ce554bf052fae4a387d3a6b
Joshua T Corbin <jcorbin@wunjo.org>
2023-12-19 10:50:48 -0500

    nvim: switch from dirvish to oil.nvim

--- .config/nvim/lua/plugins/filesystem.lua
+++ .config/nvim/lua/plugins/filesystem.lua
@@ -1,9 +1,45 @@
 -- TODO maybe 'tpope/vim-eunuch' or a lua replacement
--- TODO vinegar like mappings for netrw, or a replacement above
-
 return {
-  'justinmk/vim-dirvish',
-  dependencies = {
-    'kristijanhusak/vim-dirvish-git',
+  {
+    'stevearc/oil.nvim',
+
+    opts = {
+      -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+      -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+      -- Additionally, if it is a string that matches "actions.<name>",
+      -- it will use the mapping at require("oil.actions").<name>
+      -- Set to `false` to remove a keymap
+      -- See :help oil-actions for a list of all available actions
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    },
+
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
+
+    config = function(_, opts)
+      local oil = require 'oil'
+      oil.setup(opts)
+
+      vim.keymap.set('n', '-',
+        oil.open, { desc = "Open parent directory" })
+    end,
   },
 }
