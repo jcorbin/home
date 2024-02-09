21bc6498ab5fd465dc21642b689607cd17625166
Joshua T Corbin <jcorbin@wunjo.org>
2024-02-09 11:55:38 -0500

    nvim: drop mykeymap from telescope module

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,7 @@
-local mykeymap = require 'my.keymap'
-
 local bind = function(f, ...)
   local args = { ... }
   return function(...) return f(unpack(args), ...) end
 end
-
-local tmap = require('my.terminal').keymap
 
 return {
   'nvim-telescope/telescope.nvim',
@@ -60,35 +56,35 @@ return {
     telescope.setup(opts)
     telescope.load_extension('ui-select')
 
-    mykeymap.leader('n', 's.', telescopes.resume, { desc = 'resume last search' })
+    vim.keymap.set('n', '<leader>s.', telescopes.resume, { desc = 'resume last search' })
 
     local other_buffer = bind(telescopes.buffers, { ignore_current_buffer = true })
-    mykeymap.leader('n', '<Space>', other_buffer, { desc = 'search buffers' })
-    tmap('<Space>', other_buffer, { desc = 'search buffers' })
+    vim.keymap.set('n', '<leader><Space>', other_buffer, { desc = 'search buffers' })
+    vim.keymap.set('t', '<C-\\><Space>', other_buffer, { desc = 'search buffers' })
 
-    mykeymap.leader('n', 'sf', bind(telescopes.find_files, {
+    vim.keymap.set('n', '<leader>sf', bind(telescopes.find_files, {
       previewer = false,
       hidden = true,
       no_ignore = true,
     }), { desc = 'search files' })
 
-    mykeymap.leader('n', 'sc', bind(telescopes.find_files, {
+    vim.keymap.set('n', '<leader>sc', bind(telescopes.find_files, {
       cwd = vim.fs.dirname(vim.env.MYVIMRC),
       previewer = false,
       hidden = true,
       no_ignore = true,
     }), { desc = 'search files near $MYVIMRC' })
 
-    mykeymap.leader('n', 'sh', telescopes.help_tags, { desc = 'search help' })
-    mykeymap.leader('n', 'sm', telescopes.man_pages, { desc = 'search man pages' })
+    vim.keymap.set('n', '<leader>sh', telescopes.help_tags, { desc = 'search help' })
+    vim.keymap.set('n', '<leader>sm', telescopes.man_pages, { desc = 'search man pages' })
 
-    mykeymap.leader('n', 'sb', telescopes.current_buffer_fuzzy_find, { desc = 'search in current buffer' })
+    vim.keymap.set('n', '<leader>sb', telescopes.current_buffer_fuzzy_find, { desc = 'search in current buffer' })
 
-    mykeymap.leader('n', 'ss', telescopes.grep_string, { desc = 'grep string search' })
-    mykeymap.leader('n', 'sg', telescopes.live_grep, { desc = 'live grep search' })
+    vim.keymap.set('n', '<leader>ss', telescopes.grep_string, { desc = 'grep string search' })
+    vim.keymap.set('n', '<leader>sg', telescopes.live_grep, { desc = 'live grep search' })
 
-    mykeymap.leader('n', 'so', telescopes.oldfiles, { desc = 'search old files' })
-    mykeymap.leader('n', 'st', telescopes.treesitter, { desc = 'search syntax tree' })
-    mykeymap.leader('n', 'sd', telescopes.diagnostics, { desc = 'search diagnostics' })
+    vim.keymap.set('n', '<leader>so', telescopes.oldfiles, { desc = 'search old files' })
+    vim.keymap.set('n', '<leader>st', telescopes.treesitter, { desc = 'search syntax tree' })
+    vim.keymap.set('n', '<leader>sd', telescopes.diagnostics, { desc = 'search diagnostics' })
   end
 }
