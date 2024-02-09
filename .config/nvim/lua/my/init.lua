56259fceb1e9c87abd3090095720a8a244ed72a4
Joshua T Corbin <jcorbin@wunjo.org>
2024-02-09 11:22:40 -0500

    nvim: add mapping to edit directory of MYVIMRC

--- .config/nvim/lua/my/init.lua
+++ .config/nvim/lua/my/init.lua
@@ -16,6 +16,10 @@ mykeymap.leader('n', 'ev', function()
   vim.cmd.vsplit(vim.env.MYVIMRC)
 end, { desc = 'edit $MYVIMRC' })
 
+mykeymap.leader('n', 'ec', function()
+  vim.cmd.vsplit(vim.fs.dirname(vim.env.MYVIMRC))
+end, { desc = 'edit directory of $MYVIMRC' })
+
 autocmd('BufWritePost', vim.env.MYVIMRC, function(opts)
   vim.schedule(file_doer(opts.file))
 end)
