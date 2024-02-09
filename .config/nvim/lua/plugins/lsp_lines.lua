113a42304b48c779a223a9ebdca5a4887b83a73f
Joshua T Corbin <jcorbin@wunjo.org>
2024-02-09 12:34:09 -0500

    nvim: show less virtual lsp diagnostic lines

--- .config/nvim/lua/plugins/lsp_lines.lua
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -2,10 +2,18 @@ local mykeymap = require 'my.keymap'
 
 return {
   url = 'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-  config = function()
+
+  opts = {},
+
+  config = function(_, opts)
     local lsp_lines = require 'lsp_lines'
-    lsp_lines.setup()
-    vim.diagnostic.config({ virtual_text = false, })
+    lsp_lines.setup(opts)
+
+    vim.diagnostic.config({
+      virtual_text = true,
+      virtual_lines = { only_current_line = true },
+    })
+
     mykeymap.leader('n', 'l', lsp_lines.toggle)
   end
 }
