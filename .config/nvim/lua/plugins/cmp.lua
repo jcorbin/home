6416d49ff5a4e73381bce865b62ad95dfcc5e148
Joshua T Corbin <jcorbin@wunjo.org>
2024-02-16 08:46:31 -0500

    nvim: add snippy for snippets, integrated into completion

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -12,13 +12,27 @@ return {
       'ray-x/cmp-treesitter',
       'petertriho/cmp-git',
       'PhilRunninger/cmp-rpncalc',
+      'dcampos/cmp-snippy',
     },
 
     config = function()
+      local snippy = require('snippy')
       local cmp = require 'cmp'
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       -- TODO rework and hoist into plugin spec
       local opts = {
+
+        snippet = {
+          expand = function(args)
+            snippy.expand_snippet(args.body)
+          end,
+        },
 
         sources = cmp.config.sources({
           { name = 'emoji' },
@@ -26,6 +40,7 @@ return {
         }, {
           { name = 'nvim_lsp_signature_help' },
           { name = 'nvim_lsp' },
+          { name = 'snippy' },
         }, {
           { name = 'treesitter' },
           { name = 'buffer' },
@@ -49,6 +64,10 @@ return {
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
+            elseif snippy.can_expand_or_advance() then
+              snippy.expand_or_advance()
+            elseif has_words_before() then
+              cmp.complete()
             else
               fallback()
             end
@@ -57,6 +76,8 @@ return {
           ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
+            elseif snippy.can_jump(-1) then
+              snippy.previous()
             else
               fallback()
             end
@@ -129,5 +150,12 @@ return {
       'nvim-lua/plenary.nvim',
     },
   },
+
+  {
+    'dcampos/cmp-snippy',
+    dependencies = {
+      'dcampos/nvim-snippy',
+    },
+  }
 
 }
---
+++ .config/nvim/lua/plugins/snippy.lua
@@ -0,0 +1,20 @@
+return {
+  'dcampos/nvim-snippy',
+
+  dependencies = {
+    'honza/vim-snippets',
+  },
+
+  opts = {
+    mappings = {
+      is = {
+        ['<Tab>'] = 'expand_or_advance',
+        ['<S-Tab>'] = 'previous',
+      },
+      nx = {
+        ['<leader>x'] = 'cut_text',
+      },
+    },
+  },
+
+}
