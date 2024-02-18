712937350229cb5184b813976adeafa1dceb7b57
Joshua T Corbin <jcorbin@wunjo.org>
2024-02-17 21:25:45 -0500

    nvim: drop dead code from my.keymap
    Move space mapping out to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -70,6 +70,8 @@ vim.opt.expandtab = true
 local augroup = require 'my.augroup'
 local autocmd = augroup 'myvimrc'
 
+-- use <Space> for mapleader
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- .config/nvim/lua/my/keymap.lua
+++ .config/nvim/lua/my/keymap.lua
@@ -1,28 +1,8 @@
 local keymap = vim.keymap
 
--- use <Space> for mapleader
-keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
 local mykeymap = {}
 
 -- TODO desc support
-
--- creates a pair of mappings under the [ and ] family
-mykeymap.pair = function(mode, key, prev, next, opts)
-  local prev_opts = {}
-  local next_opts = {}
-  if opts then
-    prev_opts = vim.tbl_extend('force', prev_opts, opts)
-    next_opts = vim.tbl_extend('force', next_opts, opts)
-    local desc = opts.desc
-    if desc then
-      prev_opts = vim.tbl_extend('force', prev_opts, { desc = 'Previous ' .. desc })
-      next_opts = vim.tbl_extend('force', next_opts, { desc = 'Next ' .. desc })
-    end
-  end
-  vim.keymap.set(mode, '[' .. key, prev, prev_opts)
-  vim.keymap.set(mode, ']' .. key, next, next_opts)
-end
 
 -- keymap.set combinator that adds a LHS prefix
 mykeymap.prefix = function(prefix, mapper)
@@ -54,19 +34,5 @@ mykeymap.options = function(forced_opts, mapper)
 end
 
 mykeymap.leader = mykeymap.prefix '<Leader>'
-
-mykeymap.opt_toggle = function(keys, name)
-  vim.keymap.set('n', keys, function()
-    if vim.opt[name]:get() then
-      vim.opt[name] = false
-      vim.notify('set no' .. name)
-    else
-      vim.opt[name] = true
-      vim.notify('set ' .. name)
-    end
-  end, {
-    desc = "toggle '" .. name .. "' option"
-  })
-end
 
 return mykeymap
