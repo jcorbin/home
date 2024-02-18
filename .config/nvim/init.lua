f71cd4ca422e6bb5f236e52fdc8e76ae69db4be6
Joshua T Corbin <jcorbin@wunjo.org>
2024-02-17 20:38:42 -0500

    nvim: flatten my.diagnostics module

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,10 +166,42 @@ vim.keymap.set('t', '<C-\\>P',
   function() vim.api.nvim_paste(vim.fn.getreg('+'), false, -1) end,
   { desc = 'Paste OS' })
 
+-- diagnostic config and mappings
+vim.diagnostic.config {
+  signs = true,
+  virtual_text = true,
+  underline = true,
+  float = {
+    source = 'if_many',
+  },
+}
+
+vim.keymap.set('n', '[q', bind(vim.cmd, 'cprev'), { desc = 'Previous error (quickfix)' })
+vim.keymap.set('n', ']q', bind(vim.cmd, 'cnext'), { desc = 'Next error (quickfix)' })
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
+
+-- TODO dedupe diagnostic open mappings
+vim.keymap.set('n', '<leader>dg',
+  vim.diagnostic.open_float, { desc = 'Open diagnostics float' })
+
+vim.keymap.set('n', '<leader>dh',
+  vim.diagnostic.hide, { desc = 'Hide diagnostics' })
+
+vim.keymap.set('n', '<leader>dd',
+  function()
+    if vim.diagnostic.is_disabled() then
+      vim.diagnostic.enable()
+    else
+      vim.diagnostic.disable()
+    end
+  end,
+  { desc = 'Toggle diagnostics' })
+
 -- TODO hoist to be nearly first thing after we pull all keymaps and other order-sensitive settings out
 require('lazy').setup('plugins')
 
-require 'my.diagnostics'
 require 'my.language_servers'
 
 -- highlight yanks
