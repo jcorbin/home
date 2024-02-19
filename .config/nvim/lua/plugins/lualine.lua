fc2843192fb52c32e4c5c00823783838d781911f
Joshua T Corbin <jcorbin@wunjo.org>
2024-02-19 16:54:37 -0500

    nvim: revamp lualine, rip statusline, hello winbar

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,41 @@
+-- useful showcmd analog when using cmdheight=0 (like with noice.nvim)
+local ns = vim.api.nvim_create_namespace('showcmd_msg')
+
+local last_showcmd_msg = '⋯'
+local last_showcmd_pending
+local last_showcmd_linger = 1000
+
+vim.ui_attach(ns, { ext_messages = true }, function(event, ...)
+  if event == 'msg_showcmd' then
+    local content = ...
+
+    if last_showcmd_pending then
+      last_showcmd_pending:stop()
+      last_showcmd_pending = nil
+    end
+
+    if #content > 0 then
+      local it = vim.iter(content)
+      it:map(function(tup) return tup[2] end)
+      last_showcmd_msg = it:join('')
+    else
+      last_showcmd_pending = vim.defer_fn(function()
+        last_showcmd_msg = '⋯'
+      end, last_showcmd_linger)
+    end
+  end
+end)
+local function showcmd()
+  return last_showcmd_msg
+end
+
+local filename = {
+  'filename',
+  newfile_status = true,
+  file_status = true,
+  path = 1, -- relative path
+}
+
 return {
   'nvim-lualine/lualine.nvim',
   opts = {
@@ -14,58 +52,40 @@ return {
         winbar = 1000,
       }
     },
+
     sections = {
-      lualine_a = {
-        {
-          'filename',
-          newfile_status = true,
-          file_status = true,
-          path = 1, -- relative path
-        },
-      },
-      lualine_b = {
-        'filesize',
-        'encoding',
-        'fileformat',
-      },
-      lualine_c = {
-        'searchcount',
-        'diagnostics',
-      },
-      lualine_x = {
-        'branch',
-        'diff',
-      },
-      lualine_y = {
-        'progress',
-        'location',
-      },
-      lualine_z = {
-        'mode',
-      }
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {}
     },
     inactive_sections = {
-      lualine_a = {
-        {
-          'filename',
-          newfile_status = true,
-          file_status = true,
-          path = 1, -- relative path
-        },
-      },
-      lualine_b = {
-      },
-      lualine_c = {
-        'searchcount',
-        'diagnostics',
-      },
-      lualine_x = {
-      },
-      lualine_y = {
-        'location',
-      },
-      lualine_z = {
-      }
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
     },
+
+    winbar = {
+      lualine_a = { filename, 'location' },
+      lualine_b = { 'branch' },
+      lualine_c = { 'diff' },
+      lualine_x = { 'searchcount', 'diagnostics' },
+      lualine_y = { showcmd },
+      lualine_z = { 'mode' }
+    },
+    inactive_winbar = {
+      lualine_a = { filename, 'location' },
+      lualine_b = { 'branch' },
+      lualine_c = { 'diff' },
+      lualine_x = { 'searchcount', 'diagnostics' },
+      lualine_y = {},
+      lualine_z = {}
+    },
+
   },
 }
