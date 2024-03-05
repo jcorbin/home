fbc19738ec6590223943ad2d09cdca8b9ffea0a7
Joshua T Corbin <jcorbin@wunjo.org>
2024-03-05 18:22:18 -0500

    nvim: move telescope mappings out into init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,6 +34,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup('plugins')
 -- }}}
 
+local telescopes = require 'telescope.builtin'
+
 --- easy editing of/around $MYVIMRC {{{
 vim.keymap.set('n', '<leader>ev', function()
   vim.cmd.vsplit(vim.env.MYVIMRC)
@@ -41,6 +43,20 @@ end, { desc = 'edit $MYVIMRC' })
 vim.keymap.set('n', '<leader>ec', function()
   vim.cmd.vsplit(vim.fs.dirname(vim.env.MYVIMRC))
 end, { desc = 'edit directory of $MYVIMRC' })
+
+vim.keymap.set('n', '<leader>sc', bind(telescopes.find_files, {
+  cwd = vim.fs.dirname(vim.env.MYVIMRC),
+  previewer = false,
+  hidden = true,
+  no_ignore = true,
+}), { desc = 'search files near $MYVIMRC' })
+
+vim.keymap.set('n', '<leader>sl', bind(telescopes.find_files, {
+  cwd = vim.fn.stdpath('data'),
+  previewer = false,
+  hidden = true,
+  no_ignore = true,
+}), { desc = 'search files near $MYVIMRC' })
 
 -- auto reload $MYVIMRC after write
 autocmd('BufWritePost', vim.env.MYVIMRC, function(opts)
@@ -190,6 +206,36 @@ autocmd('FileType', { -- ...off by exception
 }, 'setlocal nospell')
 autocmd('TermOpen', 'setlocal nospell')
 -- }}}
+
+--- misc telescopes {{{
+
+vim.keymap.set('n', '<leader>s.', telescopes.resume, { desc = 'resume last search' })
+
+local other_buffer = bind(telescopes.buffers, { ignore_current_buffer = true })
+vim.keymap.set('n', '<leader><Space>', other_buffer, { desc = 'search buffers' })
+vim.keymap.set('t', '<C-\\><Space>', other_buffer, { desc = 'search buffers' })
+
+vim.keymap.set('n', '<leader>sf', bind(telescopes.find_files, {
+  previewer = false,
+  hidden = true,
+  no_ignore = true,
+}), { desc = 'search files' })
+
+vim.keymap.set('n', '<leader>sh', telescopes.help_tags, { desc = 'search help' })
+vim.keymap.set('n', '<leader>sm', telescopes.man_pages, { desc = 'search man pages' })
+
+vim.keymap.set('n', '<leader>sb', telescopes.current_buffer_fuzzy_find, { desc = 'search in current buffer' })
+
+vim.keymap.set('n', '<leader>ss', telescopes.grep_string, { desc = 'grep string search' })
+vim.keymap.set('n', '<leader>sg', telescopes.live_grep, { desc = 'live grep search' })
+
+vim.keymap.set('n', '<leader>so', telescopes.oldfiles, { desc = 'search old files' })
+vim.keymap.set('n', '<leader>st', telescopes.treesitter, { desc = 'search syntax tree' })
+vim.keymap.set('n', '<leader>sd', telescopes.diagnostics, { desc = 'search diagnostics' })
+
+vim.keymap.set('n', '<leader>C', telescopes.colorscheme, { desc = 'select colorschemes' })
+
+--- }}}
 
 --- :terminal Quality of Life {{{
 -- Analog of :!
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,7 +13,6 @@ return {
   },
   config = function()
     local telescope = require 'telescope'
-    local telescopes = require 'telescope.builtin'
     local actions = require 'telescope.actions'
     local themes = require 'telescope.themes'
 
@@ -55,45 +54,5 @@ return {
 
     telescope.setup(opts)
     telescope.load_extension('ui-select')
-
-    vim.keymap.set('n', '<leader>s.', telescopes.resume, { desc = 'resume last search' })
-
-    local other_buffer = bind(telescopes.buffers, { ignore_current_buffer = true })
-    vim.keymap.set('n', '<leader><Space>', other_buffer, { desc = 'search buffers' })
-    vim.keymap.set('t', '<C-\\><Space>', other_buffer, { desc = 'search buffers' })
-
-    vim.keymap.set('n', '<leader>sf', bind(telescopes.find_files, {
-      previewer = false,
-      hidden = true,
-      no_ignore = true,
-    }), { desc = 'search files' })
-
-    vim.keymap.set('n', '<leader>sc', bind(telescopes.find_files, {
-      cwd = vim.fs.dirname(vim.env.MYVIMRC),
-      previewer = false,
-      hidden = true,
-      no_ignore = true,
-    }), { desc = 'search files near $MYVIMRC' })
-
-    vim.keymap.set('n', '<leader>sl', bind(telescopes.find_files, {
-      cwd = vim.fn.stdpath('data'),
-      previewer = false,
-      hidden = true,
-      no_ignore = true,
-    }), { desc = 'search files near $MYVIMRC' })
-
-    vim.keymap.set('n', '<leader>sh', telescopes.help_tags, { desc = 'search help' })
-    vim.keymap.set('n', '<leader>sm', telescopes.man_pages, { desc = 'search man pages' })
-
-    vim.keymap.set('n', '<leader>sb', telescopes.current_buffer_fuzzy_find, { desc = 'search in current buffer' })
-
-    vim.keymap.set('n', '<leader>ss', telescopes.grep_string, { desc = 'grep string search' })
-    vim.keymap.set('n', '<leader>sg', telescopes.live_grep, { desc = 'live grep search' })
-
-    vim.keymap.set('n', '<leader>so', telescopes.oldfiles, { desc = 'search old files' })
-    vim.keymap.set('n', '<leader>st', telescopes.treesitter, { desc = 'search syntax tree' })
-    vim.keymap.set('n', '<leader>sd', telescopes.diagnostics, { desc = 'search diagnostics' })
-
-    vim.keymap.set('n', '<leader>C', telescopes.colorscheme, { desc = 'select colorschemes' })
   end
 }
