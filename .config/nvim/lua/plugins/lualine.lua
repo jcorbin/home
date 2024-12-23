20e8475147d3b4afd9b8b684477ab46536dd73ef
Joshua T Corbin <jcorbin@wunjo.org>
2024-12-23 09:05:52 -0500

    WIP nvim lualine CodeCompanion

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -65,6 +65,50 @@ local filename = {
   path = 1, -- relative path
 }
 
+-- local CodeCompanion = require("lualine.component"):extend()
+
+-- CodeCompanion.processing = false
+-- CodeCompanion.spinner_index = 1
+
+-- local spinner_symbols = {
+--   "⠋",
+--   "⠙",
+--   "⠹",
+--   "⠸",
+--   "⠼",
+--   "⠴",
+--   "⠦",
+--   "⠧",
+--   "⠇",
+--   "⠏",
+-- }
+-- local spinner_symbols_len = 10
+
+-- function CodeCompanion:init(options)
+--   CodeCompanion.super.init(self, options)
+--   local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+--   vim.api.nvim_create_autocmd({ "User" }, {
+--     pattern = "CodeCompanionRequest*",
+--     group = group,
+--     callback = function(request)
+--       if request.match == "CodeCompanionRequestStarted" then
+--         self.processing = true
+--       elseif request.match == "CodeCompanionRequestFinished" then
+--         self.processing = false
+--       end
+--     end,
+--   })
+-- end
+
+-- function CodeCompanion:update_status()
+--   if self.processing then
+--     self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+--     return spinner_symbols[self.spinner_index]
+--   else
+--     return nil
+--   end
+-- end
+
 return {
   'nvim-lualine/lualine.nvim',
   opts = {
@@ -93,7 +137,10 @@ return {
     inactive_sections = {
       lualine_a = { filename },
       lualine_b = { 'location' },
-      lualine_c = { 'diagnostics' },
+      lualine_c = {
+        'diagnostics',
+        -- CodeCompanion,
+      },
       lualine_x = { 'branch', 'diff' },
       lualine_y = {},
       lualine_z = {},
