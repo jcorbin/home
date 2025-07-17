d8e0ccb7a38ec957cfe6207ee06d7839a6065ab6
Joshua T Corbin <jcorbin@wunjo.org>
2025-07-17 09:12:01 -0400

    nvim: use cmp default map

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -31,7 +31,7 @@ return {
       -- C-k: Toggle signature help (if signature.enabled = true)
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'enter' },
+      keymap = { preset = 'default' },
 
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
