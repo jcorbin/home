7cd5bb22ddde5ba6a62c8e59d3684b7bb8a7ea20
Joshua T Corbin <jcorbin@wunjo.org>
2025-09-19 17:51:53 -0400

    nvim: migrate lspconfig -> vim.lsp.config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -357,6 +357,108 @@ local function popup(v)
   })
 end
 
+vim.lsp.config('bashls', {
+  filetypes = { 'sh', 'bash', 'zsh' },
+})
+
+vim.lsp.config('glslls', {
+  cmd = {
+    "glslls",
+    "--stdin",
+    "--target-env", "opengl",
+    -- [vulkan vulkan1.0 vulkan1.1 vulkan1.2 vulkan1.3 opengl opengl4.5]
+  },
+})
+
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      -- Tell the language server how to find Lua modules same way as Neovim
+      -- (see `:h lua-module-load`)
+      path = {
+        'lua/?.lua',
+        'lua/?/init.lua',
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library'
+          -- '${3rd}/busted/library'
+        }
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = {
+        --   vim.api.nvim_get_runtime_file('', true),
+        -- }
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
+
+vim.lsp.config('openscad_lsp', {
+  cmd = {
+    "openscad-lsp", "--stdio",
+    -- "--fmt-style", "file"
+  },
+})
+
+vim.lsp.enable({
+  'arduino_language_server',
+  'basedpyright',
+  'bashls',
+  'clangd',
+  'cssls',
+  'dockerls',
+  'glslls',
+  'gopls',
+  'html',
+  'jsonls',
+  'lua_ls',
+  'openscad_lsp',
+  'rust_analyzer',
+  'ts_ls',
+  'yamlls',
+  'vimls',
+  'zls',
+})
+
+vim.lsp.config('*', {
+  capabilities = require('blink.cmp').get_lsp_capabilities()
+
+  -- capabilities = {
+  --   textDocument = {
+  --     completion = {
+  --       completionItem = {
+  --         snippetSupport = true,
+  --       }
+  --     }
+  --   }
+  -- }
+
+})
+
 autocmd('LspAttach', function(ev)
   local client = vim.lsp.get_client_by_id(ev.data.client_id)
   if client == nil then return end
