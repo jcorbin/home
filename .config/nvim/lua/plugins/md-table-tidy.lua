90e63fc7d603ebcf46c8adf212695aebad2ba928
Joshua T Corbin <jcorbin@wunjo.org>
2026-02-25 22:41:03 -0500

    nvim: add md-table-tidy

---
+++ .config/nvim/lua/plugins/md-table-tidy.lua
@@ -0,0 +1,7 @@
+return {
+  "timantipov/md-table-tidy.nvim",
+  opts = {
+    padding = 1,          -- number of spaces for cell padding
+    key = "<leader>tt",    -- key for command :TableTidy<CR>
+  },
+}
