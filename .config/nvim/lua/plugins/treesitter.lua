d92009dfb7d6af88abd2d3f9dfcbb1fc7cb76832
Joshua T Corbin <jcorbin@wunjo.org>
2026-04-25 11:03:24 -0400

    WIP nvim: treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,48 @@
 return {
+
+  -- {
+  --   "arborist-ts/arborist.nvim",
+  -- },
+
   {
-    'nvim-treesitter/nvim-treesitter',
-
-    opts = {
-      ensure_installed = {},
-      sync_install = 'false',
-      auto_install = 'true',
-
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
-
-      indent = { enable = true },
-
-    },
+    "romus204/tree-sitter-manager.nvim",
+    dependencies = {}, -- tree-sitter CLI must be installed system-wide
+    config = function()
+      require("tree-sitter-manager").setup({
+        -- Optional: custom paths
+        -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+        -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+      })
+    end
   },
+
+  -- {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   branch = 'main',
+  --
+  --   opts = {
+  --     ensure_installed = {},
+  --     sync_install = 'false',
+  --     auto_install = 'true',
+  --
+  --     highlight = {
+  --       enable = true,
+  --       additional_vim_regex_highlighting = false,
+  --     },
+  --
+  --     incremental_selection = {
+  --       enable = true,
+  --       keymaps = {
+  --         init_selection = "gnn",
+  --         node_incremental = "grn",
+  --         scope_incremental = "grc",
+  --         node_decremental = "grm",
+  --       },
+  --     },
+  --
+  --     indent = { enable = true },
+  --
+  --   },
+  -- },
+
 }
