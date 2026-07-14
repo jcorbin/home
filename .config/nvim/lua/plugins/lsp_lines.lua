de3470bce3a1a238c159b466ab3038e9adbf1978
Joshua T Corbin <jcorbin@wunjo.org>
2026-07-14 10:35:26 -0400

    nvim: merge lsp diagnostic config
    Merge onto the base diagnostic config from init.lua rather than
    replacing it (which would drop signs / underline / float.source).

--- .config/nvim/lua/plugins/lsp_lines.lua
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -7,10 +7,9 @@ return {
     local lsp_lines = require 'lsp_lines'
     lsp_lines.setup(opts)
 
-    vim.diagnostic.config({
-      virtual_text = true,
+    vim.diagnostic.config(vim.tbl_extend('force', vim.diagnostic.config(), {
       virtual_lines = { only_current_line = true },
-    })
+    }))
 
     -- Bound under the <leader>l* line-toggle group (avoids shadowing that prefix).
     vim.keymap.set('n', '<leader>lL', lsp_lines.toggle, { desc = 'toggle lsp_lines diagnostics' })
