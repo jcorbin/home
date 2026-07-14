75c63cf45b89d70fcd0da681113f0efda5d3e52a
Joshua T Corbin <jcorbin@wunjo.org>
2026-07-14 10:31:10 -0400

    nvim: cleanup dead treesitter code

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,5 @@
 return {
 
-  -- {
-  --   "arborist-ts/arborist.nvim",
-  -- },
-
   {
     "romus204/tree-sitter-manager.nvim",
     dependencies = {}, -- tree-sitter CLI must be installed system-wide
@@ -15,34 +11,5 @@ return {
       })
     end
   },
-
-  -- {
-  --   'nvim-treesitter/nvim-treesitter',
-  --   branch = 'main',
-  --
-  --   opts = {
-  --     ensure_installed = {},
-  --     sync_install = 'false',
-  --     auto_install = 'true',
-  --
-  --     highlight = {
-  --       enable = true,
-  --       additional_vim_regex_highlighting = false,
-  --     },
-  --
-  --     incremental_selection = {
-  --       enable = true,
-  --       keymaps = {
-  --         init_selection = "gnn",
-  --         node_incremental = "grn",
-  --         scope_incremental = "grc",
-  --         node_decremental = "grm",
-  --       },
-  --     },
-  --
-  --     indent = { enable = true },
-  --
-  --   },
-  -- },
 
 }
